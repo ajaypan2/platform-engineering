1991db7d48c7c655829885364be4cc521314658f
Ajay Pandey <50028338+ajaypan2@users.noreply.github.com>
2025-07-28 11:48:01 +0530

    Update main.tf
    added content to connect to the cluster

--- aws-idp-terraform-step1/main.tf
+++ aws-idp-terraform-step1/main.tf
@@ -26,6 +26,15 @@ module "eks" {
   subnet_ids         = module.vpc.private_subnets
   vpc_id             = module.vpc.vpc_id
   enable_irsa        = true
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::767397705569:role/AWSReservedSSO_AWSAdministratorAccess_35990becbf6cf54f"
+      username = "admin"
+      groups   = ["system:masters"]
+    }
+  ]
 
   eks_managed_node_groups = {
     default = {
